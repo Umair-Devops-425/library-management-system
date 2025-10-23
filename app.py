7bbfa60163d46506d7c026135d5899128091a613
Umair <umairmohd425@gmail.com>
2025-10-23 21:23:01 +0530

    updating

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+import os
 from flask import Flask, g, escape, session, redirect, render_template, request, jsonify, Response
 from Misc.functions import *
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+# MySQL Configuration
+app.config['MYSQL_DATABASE_USER'] = os.environ.get('DB_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('DB_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.environ.get('DB_NAME')
+app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_DATABASE_PORT'] = int(os.environ.get('DB_PORT', 3306))
+
+mysql = MySQL(app)
+
 
 app = Flask(__name__)
 app.secret_key = '#$ab9&^BB00_.'
